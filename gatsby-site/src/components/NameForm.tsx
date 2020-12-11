220fa16f63f0361cb02f07d849ec7a00a45bcfa3
kflange <zknsftk1msrk@gmail.com>
2020-12-11 13:59:22 +0900

    init

---
+++ gatsby-site/src/components/NameForm.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+interface State {
+  value: string,
+}
+
+export default class NameForm extends React.Component<{}, State> {
+  state: State = {
+    value: '',
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label> Name: <input type="text" value={this.state.value} onChange={this.handleChange} /> </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
--- gatsby-site/src/pages/index.tsx
+++ gatsby-site/src/pages/index.tsx
@@ -2,11 +2,17 @@ import React from "react"
 // import { Link, graphql } from "gatsby";
 
 import TodoList from '../components/TodoList';
+import NameForm from '../components/NameForm';
 
 export default function Home() {
   return (
     <div style={{ margin: "2em"}}>
+      <NameForm/>
       <TodoList/>
     </div>
   );
 }
+      //<form>
+      //  <label> Name: <input type="text" name="name" /> </label>
+      //  <input type="submit" value="Submit" />
+      //</form>
